69f8cc07c9e09e8c35832ffef58ea6f359549e43
k9ordon <github@k94n.com>
2015-09-15 21:28:02 +0200

    momentjs

--- lemeteor/components/messageList.js
+++ lemeteor/components/messageList.js
@@ -5,6 +5,7 @@ if(Meteor.isClient) {
                 .find({}, {limit:10, sort:{date:-1}})
                 .map(function(message) {
                     message.user = Meteor.users.findOne({_id:message.user_id});
+                    message.moment = moment(message.date).fromNow();
                     return message;
                 });
         }
