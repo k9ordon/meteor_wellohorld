964ea612e2daac316ae069300063a507a2a4700c
k9ordon <github@k94n.com>
2015-09-15 22:20:28 +0200

    semanticui

--- lemeteor/components/messageList.js
+++ lemeteor/components/messageList.js
@@ -4,7 +4,9 @@ if(Meteor.isClient) {
             return Messages
                 .find({}, {limit:10, sort:{date:-1}})
                 .map(function(message) {
-                    message.user = Meteor.users.findOne({_id:message.user_id});
+                    var user = Meteor.users.findOne({_id:message.user_id});
+
+                    message.username = user.username || user.profile.name;
                     message.moment = moment(message.date).fromNow();
                     return message;
                 });
