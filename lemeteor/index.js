d4b5ddf847f45b1165c318df264fd504a75b1830
k9ordon <github@k94n.com>
2015-09-15 20:45:28 +0200

    router

--- lemeteor/index.js
+++ lemeteor/index.js
@@ -1,1 +1,13 @@
 Messages = new Mongo.Collection('messages');
+
+Router.configure({
+    layoutTemplate: 'layout'
+})
+
+Router.route('/', function() {
+    this.render('Index', {data: {lol:5}});
+});
+
+Router.route('/messages', function() {
+    this.render('messages', {});
+});
